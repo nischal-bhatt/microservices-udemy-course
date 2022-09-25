0a85a5313b410dd790d28d2dbfd08e537ab14398
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-09-25 21:16:53 +0800

    creating docker image now using buildpacks for loans

--- loans-micro/src/main/java/com/eazybytes/loans/context/RequestContext.java
+++ loans-micro/src/main/java/com/eazybytes/loans/context/RequestContext.java
@@ -2,16 +2,30 @@ package com.eazybytes.loans.context;
 
 import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-import lombok.Setter;
+
 
 @Component
-@Getter
-@Setter
+
 public class RequestContext {
 
 	public static final String CORRELATION_ID = "eazybank-correlation-id";
 
 	private String correlationId = new String();
 
+	public String getCorrelationId() {
+		return correlationId;
+	}
+
+	public void setCorrelationId(String correlationId) {
+		this.correlationId = correlationId;
+	}
+
+	
+
+	@Override
+	public String toString() {
+		return "RequestContext [correlationId=" + correlationId + "]";
+	}
+
+	
 }
--- loans-micro/src/main/java/com/eazybytes/loans/model/Loans.java
+++ loans-micro/src/main/java/com/eazybytes/loans/model/Loans.java
@@ -8,12 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 
 @Entity
-@Getter @Setter @ToString
+
 public class Loans {
 
 	@Id
@@ -41,5 +39,77 @@ public class Loans {
 	
 	@Column(name = "create_dt")
 	private String createDt;
+
+	public int getLoanNumber() {
+		return loanNumber;
+	}
+
+	public void setLoanNumber(int loanNumber) {
+		this.loanNumber = loanNumber;
+	}
+
+	public int getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+
+	public Date getStartDt() {
+		return startDt;
+	}
+
+	public void setStartDt(Date startDt) {
+		this.startDt = startDt;
+	}
+
+	public String getLoanType() {
+		return loanType;
+	}
+
+	public void setLoanType(String loanType) {
+		this.loanType = loanType;
+	}
+
+	public int getTotalLoan() {
+		return totalLoan;
+	}
+
+	public void setTotalLoan(int totalLoan) {
+		this.totalLoan = totalLoan;
+	}
+
+	public int getAmountPaid() {
+		return amountPaid;
+	}
+
+	public void setAmountPaid(int amountPaid) {
+		this.amountPaid = amountPaid;
+	}
+
+	public int getOutstandingAmount() {
+		return outstandingAmount;
+	}
+
+	public void setOutstandingAmount(int outstandingAmount) {
+		this.outstandingAmount = outstandingAmount;
+	}
+
+	public String getCreateDt() {
+		return createDt;
+	}
+
+	public void setCreateDt(String createDt) {
+		this.createDt = createDt;
+	}
+
+	@Override
+	public String toString() {
+		return "Loans [loanNumber=" + loanNumber + ", customerId=" + customerId + ", startDt=" + startDt + ", loanType="
+				+ loanType + ", totalLoan=" + totalLoan + ", amountPaid=" + amountPaid + ", outstandingAmount="
+				+ outstandingAmount + ", createDt=" + createDt + "]";
+	}
+	
 	
 }
